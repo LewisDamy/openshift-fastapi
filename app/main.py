ab23aad84efd101e6960a8715d946acbd8935b80
Lewis_Damy <ldamy16@gmail.com>
2024-07-18 15:30:50 -0300

    ⚡️ Modify main.py file for deployment

--- app/main.py
+++ app/main.py
@@ -2,8 +2,15 @@ from fastapi import FastAPI
 from app.routers import users
 from app.database import engine, Base
 
-Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+def create_app():
+    Base.metadata.create_all(bind=engine)
 
-app.include_router(users.router)
+    app = FastAPI()
+
+    app.include_router(users.router)
+
+    return app
+
+
+app = create_app()
