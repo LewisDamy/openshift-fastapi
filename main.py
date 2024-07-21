f3b7a2761801929170b031c640304b9ef8f3fd90
Lewis_Damy <ldamy16@gmail.com>
2024-07-20 21:26:34 -0300

    fix hole imports

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from routers import users
-from database import engine, Base
+from app.routers import users
+from app.database import engine, Base
 
 
 def create_app():
